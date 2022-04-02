1ded12e9bc975962b9f6047a181dc1186d1eceb9
Niklas <grim.trickster04@gmail.com>
2022-04-02 12:32:48 +0200

    refactoring + comments

--- Asteroiden Game/main.py
+++ Asteroiden Game/main.py
@@ -3,49 +3,60 @@ import matplotlib.pyplot
 
 import time
 
-#pixels
-
+#Position data
 pixels = []
+
+#Temporary horizontal data
 pixel_y = []
-pixelmode = 0
-#0 = Nichts
-#1 = Asteroid
 
-#player
-playerx = 9
-playery = 5
+#Temporary variable 0 = Nichts 1 = Asteroid
+pixelMode = 0
 
 
+#Player (Start) Coordinates
+playerX = 9
+playerY = 5
+
+
+#Sets Player at start-position
 def setPlayer():
-    pixels[playerx][playery] = 2
+    pixels[playerX][playerY] = 2
 
 
+#Generate random Game Data
 def gameGenerator():
     global pixel_y
+
+    # multiple horizontal lines
     for x in range(0, 10):
+        # 1 horizontal line
         for y in range(0, 10):
-            # pixel_mode = numpy.random.randint(0, 2)
+            # Asteroid or not (with probability)
             pixel_mode = numpy.random.choice([0, 1], p=[0.7, 0.3])
 
+            #set temporary horizontal line
             pixel_y.append(pixel_mode)
 
+        #Add all lines to big data
         pixels.append(pixel_y)
         pixel_y = []
     setPlayer()
 
-def nextStep(playerStep):
-    global playery
 
-    lastPixel = pixels[9]
+def nextStep(playerStep):
+    #To change variables outside functions: global
+    global playerY
+
+    lastPixels = pixels[9]
     for i in range(0, 9):
         pixels[9 - i] = pixels[8 - i]
-    pixels[0] = lastPixel
+    pixels[0] = lastPixels
 
     #Welche Richtung?
     if playerStep == 1:
-        playery += 1
+        playerY += 1
     elif playerStep == -1:
-        playery -= 1
+        playerY -= 1
     setPlayer()
     checkCollission()
 
